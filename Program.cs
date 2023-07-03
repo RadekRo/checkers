245e0cef2904eb43065eb7da3a6493464255ec93
RadekRo <r.roclawski@ankara.pl>
2023-07-03 22:21:33 +0200

    Created base for checkers project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace checkers
+{
+    internal class Program
+    {
+        static int boardSize = 8;
+        static void Main(string[] args)
+        {
+            bool gamePlay = false;
+
+            Console.WriteLine("Checkers Game");
+            Console.WriteLine("Juta Kozińska, Grzegorz Łabojko, Radosław Rocławski");
+            while (gamePlay)
+            {
+                /// main game loop curently set to unactive via gamePlay = false;
+            }
+        }
+    }
+}
